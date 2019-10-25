ce6a88e904a9027041d0577a0a050dd7717eb42d
Matt Tognetti <mjtognetti@gmail.com>
2019-10-24 18:57:49 -0700

    More tests

--- tests/Traverse.test.ts
+++ tests/Traverse.test.ts
@@ -3,6 +3,18 @@ import Automerge from "automerge"
 import * as Traverse from "../src/Traverse"
 
 test("Test Traverse.ts", function(t) {
+  t.test("Traverses objects and arrays", function(t) {
+    t.plan(1)
+    const root = { foo: "a", bar: ["a", "b", { baz: "a" }] }
+    const results = Traverse.iterativeDFS(root, (val: any) => val === "a")
+    t.equals(results.length, 3)
+  })
+  t.test("Selects keys as well as values", function(t) {
+    t.plan(1)
+    const root = { foo: "a", a: "ok" }
+    const results = Traverse.iterativeDFS(root, (val: any) => val === "a")
+    t.equals(results.length, 2)
+  })
   t.test("Test Automerge.Text isn't traversed", function(t) {
     t.plan(1)
     const doc = Automerge.change(Automerge.init(), function(doc: any) {
